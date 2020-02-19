b9cbea9c46685c9a1bbe91b0a3c233dd3ebdb6f5
TLLong <tllongdev@gmail.com>
2020-02-19 16:57:57 -0600

    updated bubblesort.spec.js

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -3,11 +3,11 @@ describe('Bubble Sort', function() {
     expect(bubbleSort([])).toEqual([]);
   });
 
-  it('handles a single item', function() {
+  it('handles an array with only 1 element', function() {
     expect(bubbleSort([6])).toEqual([6]);
   });
 
-  it('handles multiple items', function() {
+  it('handles an array with multiple elements', function() {
     expect(bubbleSort([9, 6, 22, 666, 555, 99, 3])).toEqual([3, 6, 9, 22, 99, 555, 666]);
   });
 });
